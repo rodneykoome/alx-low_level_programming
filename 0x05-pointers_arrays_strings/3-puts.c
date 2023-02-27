efdb125dd855e8df9d0655d1da15c308425cb935
Rodney Koome <rodneykoome@gmail.com>
2023-02-27 12:43:18 -0800

    Puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * _puts - a function that prints a sring followed by a new line.
- * @str: the string to be  printed
- * Return the string
+ * _puts - Prints a string
+ * @str: The string to print
+ *
+ * Return: void
  */
-
 void _puts(char *str)
 {
-	while(*str != '\0')
-	{
-		_putchar(*str++);
-	}
-	_putchar('\n');
+for (; *str != '\0'; str++)
+{
+_putchar(*str);
+}
+
+_putchar('\n');
 }
