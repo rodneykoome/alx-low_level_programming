08c1a98350855df49b132e0a05b67f7879fd2917
Rodney Koome <rodneykoome@gmail.com>
2023-04-10 11:40:38 -0700

    This is File I/O

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -14,37 +14,31 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
-	ssize_t lenr, lenw;
-	char *buffer;
+int fd;
+ssize_t lenr, lenw;
+char *buffer;
 
-	if (filename == NULL)
-	return (0);
-
-	fp = open(filename, O_RDONLY);
-
-	if (fp == -1)
-	return (0);
-	buffer = malloc(sizeof(char) * letters);
-
-	if (buffer == NULL)
-	{
-	close(fp);
-	return (0);
-	}
-	lenr = read(fp, buffer, letters);
-	close(fp);
-
-	if (lenr == -1)
-	{
-	free(buffer);
-	return (0);
-	}
-
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-	free(buffer);
-
-	if (lenr != lenw)
-	return (0);
-	return (lenw);
-}i
+if (filename == NULL)
+return (0);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+{
+close(fd);
+return (0);
+}
+lenr = read(fd, buffer, letters);
+close(fd);
+if (lenr == -1)
+{
+free(buffer);
+return (0);
+}
+lenw = write(STDOUT_FILENO, buffer, lenr);
+free(buffer);
+if (lenr != lenw)
+return (0);
+return (lenw);
+}
